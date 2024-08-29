43456f5d6cc942ff1b4720633136726eeb3a9400
rajan.kumar <rajan.kumar@arthmate.co.in>
2024-08-29 12:57:02 +0530

    MLOPS

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def predict_datapoint():
 
 if __name__=="__main__":
     # app.run(host="0.0.0.0",port=8080)        
-    app.run(host='0.0.0.0', port=8080)        
+    app.run(host='172.31.14.96', port=8080)        
