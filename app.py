25ace2cf994c4bb9066e95b4bace9cfb4f7f536a
rajan.kumar <rajan.kumar@arthmate.co.in>
2024-08-29 12:42:34 +0530

    MLOPS

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
+
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
@@ -27,21 +28,17 @@ def predict_datapoint():
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-
-        )
+            writing_score=float(request.form.get('reading_score')))
+        
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
-        print("Before Prediction")
 
         predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
         return render_template('home.html',results=results[0])
+    
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0" ,port = '8080')        
-
-
+    # app.run(host="0.0.0.0",port=8080)        
+    app.run(host='0.0.0.0', port=8080)        
